277f6758a246976f3ae66fcb920dd3ec07ab8604
hussinmarks <hussinmarks6@gmail.com>
2024-05-24 16:13:48 -0400

    updated

--- Marks-Portfolio/src/components/Hero/Hero.jsx
+++ Marks-Portfolio/src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/marks-portfolio.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
